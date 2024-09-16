e62cf594c38bab7fe7a93b6998eae36c198f526b
Thabiso <thabisongubanee@gmail.com>
2024-09-16 12:57:00 +0200

    Add project files.

---
+++ Payment_Using_PayfastAPI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Payment_Using_PayfastAPI.Models;
+using System.Diagnostics;
+
+namespace Payment_Using_PayfastAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Payment_Using_PayfastAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Payment_Using_PayfastAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
